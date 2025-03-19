5038747ab93b6ddd3190fef124b54abcbb99f7ae
BrunoFD84 <brunofdomingues84@gmail.com>
2025-03-19 16:48:51 +0000

    first version of MVP with docker compose

---
+++ simpleAPI/MongoDBContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace simpleAPI
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["MongoDB:ConnectionString"];
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(configuration["MongoDB:DatabaseName"]);
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection(string collectionName) =>
+            _database.GetCollection<BsonDocument>(collectionName);
+    }
+}
---
+++ simpleAPI/Services/MongoService.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace simpleAPI.Services
+{
+    public class MongoService
+    {
+        private readonly IMongoCollection<BsonDocument> _collection;
+
+        public MongoService(MongoDBContext dbContext)
+        {
+            //salvo erro se não existir é criado?
+            _collection = dbContext.GetCollection("JsonStorage");
+        }
+
+        public async Task<string> InsertJsonAsync(string json)
+        {
+            var document = BsonDocument.Parse(json);
+            await _collection.InsertOneAsync(document);
+
+            return document["_id"].ToString();
+        }
+    }
+}
